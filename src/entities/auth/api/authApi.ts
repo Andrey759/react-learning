e231559df9ad9c0646f40eeaefe48a8c73e5bf66
Андрей Ребров <ar@nextbi.ru>
2026-03-23 17:28:44 +0300

    Hide api key

--- src/entities/auth/api/authApi.ts
+++ src/entities/auth/api/authApi.ts
@@ -5,7 +5,7 @@ export type LoginResponse = { token: string };
 export async function loginRequest(email: string, password: string): Promise<LoginResponse> {
     const response = await fetch('https://reqres.in/api/login', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json', 'x-api-key': '...' },
+        headers: { 'Content-Type': 'application/json', 'x-api-key': import.meta.env.VITE_API_KEY },
         body: JSON.stringify({ email, password }),
     });
 
