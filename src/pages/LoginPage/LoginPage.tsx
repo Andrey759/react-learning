4776c54c1cbbc888e167671a422c1b2ca0f629fa
Андрей Ребров <ar@nextbi.ru>
2026-03-25 22:15:59 +0300

    clearing

--- src/pages/LoginPage/LoginPage.tsx
+++
@@ -1,8 +0,0 @@
-function LoginPage() {
-  return (
-    <div className="page-placeholder">
-      <h2>Login (coming soon)</h2>
-      <p>Форму логина добавим на следующих этапах учебного проекта.</p>
-    </div>
-  );
-}
--- src/widgets/task-details-panel/TaskDetailsPanel.tsx
+++
@@ -1,8 +0,0 @@
-function TaskDetailsPanel() {
-  return (
-    <div className="widget-placeholder">
-      <h3>Task details panel (coming soon)</h3>
-      <p>Здесь будет панель с подробностями выбранной задачи.</p>
-    </div>
-  );
-}
